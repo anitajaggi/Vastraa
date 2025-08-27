e64487a304c2df1dfa5d2a0d16f2e9abed99ccfc
Anita jaggi <164060438+anitajaggi@users.noreply.github.com>
2025-08-27 13:14:08 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -59,6 +59,9 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  path: "/",
   });
 
   res.json({
