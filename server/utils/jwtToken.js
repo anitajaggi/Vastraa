54fa2383c6f189b85884a0294cc172e4e842832c
Anita jaggi <164060438+anitajaggi@users.noreply.github.com>
2025-08-27 14:20:07 +0530

    Update jwtToken.js

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -4,17 +4,15 @@ const jwtToken = (res, user) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-<<<<<<< HEAD
-    path: "/"
-=======
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     path: "/",
->>>>>>> 77c5936 (your message here)
   });
+
   return token;
 };
 
